db593408d781ab2644ec1d1827bfca7ef930a8f2
Ismail Uras <ismailuras09@gmail.com>
2024-05-18 04:56:06 +0300

    feat:/swiper slider

---
+++ src/components/slider/slideData.js
@@ -0,0 +1,27 @@
+import container from "../../assets/container.png";
+import container2 from "../../assets/container2.png";
+import container3 from "../../assets/container3.png";
+
+export const slideData = [
+  {
+    id: 1,
+    title: "Lorem ipsum dolor sit amet dolor amet",
+    subtitle:
+      "consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    img: container,
+  },
+  {
+    id: 2,
+    title: "Sit amet dolor amet",
+    subtitle:
+      "consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    img: container2,
+  },
+  {
+    id: 3,
+    title: "Dolor sit amet dolor amet",
+    subtitle:
+      "consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    img: container3,
+  },
+];
---
+++ src/components/swiper/index.jsx
@@ -0,0 +1,60 @@
+import { arrayOf, shape, number, string } from "prop-types";
+
+import { Navigation, Pagination, Autoplay, Parallax } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+function CustomSlider({ data }) {
+  if (!data) return null;
+
+  return (
+    <section>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay, Parallax]}
+        navigation
+        pagination={{ clickable: true }}
+        onPaginationRender={(e) => e.pagination.el.classList.add("container")}
+        loop
+        speed={2000}
+        autoplay={{ delay: 5000 }}
+        parallax
+      >
+        {data.map((slideItem) => (
+          <SwiperSlide key={slideItem.id}>
+            <div className="bg">
+              <img src={slideItem.img} alt={slideItem.title} />
+            </div>
+            <div className="container" data-swiper-parallax={800}>
+              <div className="row">
+                <div className="col-6">
+                  <div className="content">
+                    <div>
+                      <h2>{slideItem.title}</h2>
+                      <p>{slideItem.subtitle}</p>
+                    </div>
+                    <a href="#">İletişime Geç</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+}
+
+CustomSlider.propTypes = {
+  data: arrayOf(
+    shape({
+      id: number.isRequired,
+      title: string.isRequired,
+      subtitle: string.isRequired,
+      img: string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default CustomSlider;
