7e0921582122255f6907ddeb5b400abde13533b7
Ismail Uras <ismailuras09@gmail.com>
2024-05-18 07:59:14 +0300

    feat:/services

---
+++ src/mocks/serviceData.js
@@ -0,0 +1,58 @@
+export const serviceData = [
+  {
+    id: 1,
+    title: "Karayolu Taşımacılığı",
+    desc: "Avrupa başta olmak üzere dünyadaki birçok noktaya karayolu taşımacılığı hizmeti sunmaktayız",
+    img: "https://s3-alpha-sig.figma.com/img/5102/a00e/26ccde393f5926317c5939f4ea936807?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pb~gB01iaFCrL6ohRL4GmE1lxyqwXzteYFIEi0gjFnR8eQPceeoa~~XKzM3n0rmOXVWRwz2eZS-ulYKFl0humBGp8FjFutplSWPNXNmEVVz6H242GAq975NfJgNeGv9sCxDpC2usZt5GhGdQUSAJ9Q51XYdXdvQrpjjitS16XtYkRtE8Dqjw9FGklNsf0p011vE6GPF59XzuiJY5MkzbwWSaHE5e6HfpIqbkWaEsaY~IJP9vPh5x2gbTObIfN7whXkwHdI0aihmnHvB-peKccB4mztFapHDdOksPQu9wj6~pXsLurZfWETaaIFjdYRLLQpZKDoYoCJhrKKlalpaU7g__",
+    url: "#",
+  },
+  {
+    id: 2,
+    title: "Havayolu Taşımacılığı",
+    desc: "Dünyanın önemli şehirlerine havayolu taşımacılığı hizmetimizle ulaşabilirsiniz.",
+    img: "https://s3-alpha-sig.figma.com/img/63fd/6237/9c19a190850118a3104051f518ef0370?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DMh5yrjyIvFjZrCC3UMuf7d3O4-H7nDO5OREZqQDU36Mwv1GaDLz8eyB2n-S9gyXImR7l9OSSVFJNRdZTwlGKQoYxWjEDrv53wxkUW2VHrCdTwayR0ruB5vxbNTq96FLN3y1ElR-6ST2vEjTJtqtlk~84UGWH6NFhhlaetsKVQ1vvualMsNbVGK3tBTBCbMAzNCW6PR~Uak1eMjHjXM8p6TLtYwmUXKbo7ZwfapuPv78gLWw86qnI~9sUndKpm9tIMHUbSesdoWmDogD-JGggk4r5FGbE2mlCgID~IuajnjT~WW~id~F-l7Ks6moCFsKwQ1Ad5r~YwS5BF5eaXZsOw__",
+    url: "#",
+  },
+  {
+    id: 3,
+    title: "Denizyolu Taşımacılığı",
+    desc: "Dünyadaki birçok limana ulaşabileceğiniz denizyolu taşımacılığı hizmeti sunmaktayız.",
+    img: "https://s3-alpha-sig.figma.com/img/0193/4fd6/33dd739883fdcd3170e132d8f5e709c6?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HKlXuqumq9TVnes5N5ObOx0vvs~HFExdbq34yee145QB6IYUyn43Se6ueByvBB9UgcHoiHfM1EkDBkTBJQIVTlWZkFIPW5QKviO-f6Y0komr5U3dAVG6geGD5PN34LbyhCe1fvx~eL32~Z5siFcr4SENjqYxX0lV7PTgwUQ1qNIAjYOu7Lev-jR6ozhKTRv1sLM6Vc0TP3G8WHt52S3A1aOvj-rCe1lYeJHnbYCDiIumvvs2Z8Gn7fO0SSwOmpeVu1EYIEhZN9AItekO9CtAbOC0rZD2B01A5NmiHL6UXre3NVxstwuuMEfZpB5IeNjDY8R0l-Mrvd0y8R7fylunQg__",
+    url: "#",
+  },
+  {
+    id: 4,
+    title: "Demiryolu Taşımacılığı",
+    desc: "Dünyadaki birçok önemli merkeze demir yolu taşımacılığı hizmeti sunmaktayız.",
+    img: "https://s3-alpha-sig.figma.com/img/a9ca/4de6/6dc926cac376c9b7a6ec144630cf167a?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jcVXCsYN4tbXvelRR3HR5R5AGbjXoWATQfKpwsG91bnzcJX3he127k0djoVmjEPwiDlZIUtKPhUJBs85f8wNXRyMf~akgVo05mxKtvq0xT63HH3YBv7h-avjMWUADtR8A9wsY9P6TqM2SmVcjxUMRxIX~lxvGrML8NKErvRoqhecH4Ot2asZ328v4ZNkToGcvGmJXiUT0jpCC6ZdN9qcezBBCH5EJIgmtGAKW67-~fp10gYzNcDOb9wjhvi5u5WojrROPvUdkc-5t-3OrcZahpPFS-gbc94hPuMztdt8ofTe~ZGFkohMK3LAQMXyn-ONdjoYaVATNzU4bqhFVDJBRA__",
+    url: "#",
+  },
+  {
+    id: 5,
+    title: "E-Ticaret Lojistiği",
+    desc: "E-ticaret uygulamalarınızda, Enco olarak kaliteli ve güvenilir hizmetimizle yanınızdayız.",
+    img: "https://s3-alpha-sig.figma.com/img/db92/40f8/9d22bbec50742ffa65ffbb197534baa4?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kryaTJ79UzZcJO3736v2VTbn12DI~G6ioomCHFbteiujTWoqR2aZS~B95Vs7WITii~wNAijYwulHmnSgeiWHgubI1g~wQwH2xrUk2oqfWBjuGmNNwlaaNTrCbfBh9ha0ascEHvjhPYoys60XfQafqfe-bny7pdYXLJoTcdo9oWeeoXl-ynz4br3qwU~4DTBK1ViBm76YX~t8G-6m7KrpMUy~sThajadE1RBLNC-NBi~o6mZfvM-nuIdkwxNy45DsC28Ph~tVwBO4gMQhumXCquF~UKsaGeFxzQefYqQRoe1c4jAdG8hofi25BZVa9XX6OJedgkbKsJEu-KYzArwbfw__",
+    url: "#",
+  },
+  {
+    id: 6,
+    title: "Depolama Hizmetleri",
+    desc: "Türkiye ve Almanya’da bulunan depolarımızla depo yönetimi süreçlerinize destek oluyoruz.",
+    img: "https://s3-alpha-sig.figma.com/img/9ce9/97ba/961858be06e4e7cbffbb158ffd793bfa?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Ve~6DwYRoxSxvOD~txnAH2C4t~1R7tRHv4fxvziNy6vnDPX01pYgLk~vE6PITBxjQnagn1VwuKQTgi88jxfqU5WX8Ff3-LNamBFxeas-LKFH3P6HqcZ1~MENRSlhRIJ1jsye-rMSTEqGS1SkMcBNFopW1iN8gmtqyFpc5E4HdL~BZoDtW9qM-4J25O5HqI1kxFkC5ZpejnD32HANBn7gOg9~PTfCONWmgRUvNszo8KNxCeTVtXze1E8dnTSdvl4wVyx7rb0WxalWhCjr~u8nH0Wx2VCxWIkynavu5jaZuURpjlPUEeeIQR5H8o7H8IpsZlXeqVj3TB1-C9hgpYr5sA__",
+    url: "#",
+  },
+  {
+    id: 7,
+    title: "Sprint Taşımacılığı",
+    desc: "Avrupa başta olmak üzere dünyadaki birçok noktaya karayolu taşımacılığı hizmeti sunmaktayız",
+    img: "https://s3-alpha-sig.figma.com/img/7698/de1d/fa1c7bc4eaf4a7d58c4937a6c1a6e133?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Pcx6~Dpi6DxyWsipJdVQ8qibnen1kWrCvlAUJWgLK9GTL2w9nGOnvelFUtLU2awn29S6LF0bxri8cHTNRcj~~LiAKiMYAZauiNBaqX-fKYpBfP7mx3dfSSpoiZbzZI8N7EU548lienSUisaFnjVAnfgKySoS304MWv0dYZn~3c9XHjIc~54Aaja~00bjME917VETlKMRwOAaQOAnySArDrklamPTzlSX5fevlZp4LDo0oWzd-gmWdkPAAQVXRKPZn-Uf8a1qljgKH~xPoKbmQCX4P-eJDPZiA7KpzW509DIfXeGRTPBdYYKlGccDUYrZv8avg5trYySJuw7hKDdtvg__",
+    url: "#",
+  },
+  {
+    id: 8,
+    title: "Kontrat Lojistiği",
+    desc: "Her türlü lojistik çözümünde Kontrat Lojistiği ile yanınızdayız.",
+    img: "https://s3-alpha-sig.figma.com/img/982e/1497/09befad45089bb5137eb9a7feae2e67a?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lauOIGlZw2HNWdQeESsf-yO~NKAhaLw3jk1KgVN~GmIQsnxfUbHnzhYN~YNLPz1zKgAg12j1eWxNSbEYT0Sa1p0KKiXLLTKNQ-jusk29rvR9Mt9~VpC1sGd~w-QxA50umjN5FUm~OD3UkYBZnSjvzkq5M0XMBHUOTTje4PGPfqwGlfCitKgU~DK2zcpw97xLAX2jH-JoIwgP4wtf7574O1oc0YZNUt9AY7vHM4bsx79i0eXw3VBBdGg3h446xxZkDXn0Rq5venTTgEx~rn8Z6oWoNYZGmWN8LokBrIE9K1xN2vuTUT4u9KrIyGBve6NqqMLlIqWz2Uk18S89tS0mWg__",
+    url: "#",
+  },
+];
